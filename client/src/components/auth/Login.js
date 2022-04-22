ee950730af16198606499cfe39656f289943806f
Ankurjainaj <ankurjainaj1234@gmail.com>
2022-04-22 15:15:27 +0530

    Added user authentication connections in frontend and backend

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -15,8 +18,14 @@ const Login = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    console.log('Success');
+    login(email, password);
   };
+
+  //Redirect if Logged in:
+  if (isAuthenticated) {
+    return <Navigate to='/dashboard' />;
+  }
+
   return (
     <Fragment>
       <h1 className='large text-primary'>Sign In</h1>
@@ -53,4 +62,12 @@ const Login = () => {
   );
 };
 
-export default Login;
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+export default connect(mapStateToProps, { login })(Login);
